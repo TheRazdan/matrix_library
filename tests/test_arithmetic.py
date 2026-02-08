62123921a4f198ebbf63ac7ffdf752350fefcc0e
TheRazdan <itopititop@gmail.com>
2026-02-08 11:40:06 +0300

    nerobotaet

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -6,3 +6,43 @@ from matrix_operations import (
     subtract_matrices,
     add_matrices
 )
+
+def test_add_matrix():
+    result = add_matrices([[1,2], [3,4]], [[5,6], [7,8]])
+    assert result == [[6,8],[10, 12]]
+
+def test_add_matrices_different_sizes():
+    with pytest.raises(TypeError):
+        add_matrices([[1,2], [3,4], [5,6]])
+
+# ----------------------------------------------------------
+
+def test_subtract_matrices():
+    result = subtract_matrices([[5,6], [7,8]], [[1,2], [3,4]])
+    assert result == [[4,4],[4,4]]
+
+def test_subtract_matrices_different_size():
+    with pytest.raises(ValueError):
+        subtract_matrices([[1]], [[1,2]])
+
+# ----------------------------------------------------------
+
+def test_multiply_by_scalar():
+    result = multiply_by_scalar([[1,2],[3,4]], 2)
+    assert result == [[19, 22], [43,50]]
+
+# ----------------------------------------------------------
+
+def test_multiply_matrices():
+    result = multiply_matrices([[1,2], [3,4]], [[5,6], [7,8]])
+    assert result == [[19,22], [43,50]]
+
+def test_multiply_matrices_incompatible_size():
+    with pytest.raises(TypeError):
+        multiply_matrices([[1,2]], [[3], [4]])
+
+# ----------------------------------------------------------
+
+def test_transpose():
+    result = transpose([[1, 2, 3],[4, 5, 6]])
+    assert result == [[1, 4],[2, 5],[3, 6]]
